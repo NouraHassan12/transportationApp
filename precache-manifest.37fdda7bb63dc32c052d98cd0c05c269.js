668ee7ad2d2f8ba357eec891bc267bc0ea6f7352
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-11-07 08:58:26 +0200

    Updates

---
+++ precache-manifest.37fdda7bb63dc32c052d98cd0c05c269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4628121bc1805650cb1421726255928d",
+    "url": "/transportationApp/index.html"
+  },
+  {
+    "revision": "08aa4dcdf1753703efbd",
+    "url": "/transportationApp/static/css/2.2791e197.chunk.css"
+  },
+  {
+    "revision": "e8991128e33c0506e021",
+    "url": "/transportationApp/static/css/main.d9c9b9d8.chunk.css"
+  },
+  {
+    "revision": "08aa4dcdf1753703efbd",
+    "url": "/transportationApp/static/js/2.3787159f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/transportationApp/static/js/2.3787159f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8991128e33c0506e021",
+    "url": "/transportationApp/static/js/main.099bd054.chunk.js"
+  },
+  {
+    "revision": "96f95ceeb4f357b33454",
+    "url": "/transportationApp/static/js/runtime-main.a3169409.js"
+  }
+]);
